397ac69603fb6fb9cbb6cd8910bd28c13ab6e277
Gunes Acar <gunes.acar@esat.kuleuven.be>
2014-09-17 16:16:11 -0700

    Add new tests

--- fingerprinting/screen.js
+++ fingerprinting/screen.js
@@ -1,33 +1,32 @@
 (function () {
     "use strict";
-    var tests = {};
-    tests["screen_saw_wiw"] = function () {
-        return screen.availWidth === window.innerWidth;
-    };    
-    tests["screen_sah_wih"] = function () {
-        return screen.availHeight === window.innerHeight;
-    };    
-    tests["screen_sw_saw"] = function () {
-        return screen.width=== screen.availWidth;
-    };
-    tests["screen_sh_sah"] = function () {
-        return screen.height === screen.availHeight;
-    };
-    tests["screen_wiw_wow"] = function () {
-        return window.innerWidth === window.outerWidth;
-    };    
-    tests["screen_wih_woh"] = function () {
-        return window.innerHeight === window.outerHeight;
-    };
-    tests["screenX"] = screenX;
-    tests["screenY"] = screenY;
-    tests["mozInnerScreenX"] = mozInnerScreenX;
-    tests["mozInnerScreenY"] = mozInnerScreenY;
-    tests["screen.left"] = screen.left;
-    tests["screen.top"] = screen.top;
-    tests["screen.availLeft"] = screen.availLeft;
-    tests["screen.availTop"] = screen.availTop;    
-    tests["screen.width"] = screen.width;
-    tests["screen.height"] = screen.height;
+    var tests = {},
+                get
+    get = function(x){
+        return x;
+    }
+    
+    tests["screen.availHeight"] = get(screen.availHeight);
+    tests["screen.availLeft"] = get(screen.availLeft);
+    tests["screen.availTop"] = get(screen.availTop);    
+    tests["screen.availWidth"] = get(screen.availWidth);
+    
+    tests["screen.height"] = get(screen.height);
+    tests["screen.left"] = get(screen.left);    
+    tests["screen.top"] = get(screen.top);
+    tests["screen.width"] = get(screen.width);
+    
+    tests["screen.colorDepth"] = get(screen.colorDepth);
+    tests["screen.pixelDepth"] = get(screen.pixelDepth);
+    
+    tests["screen.mozOrientation"] = get(screen.mozOrientation);
+    tests["screen.onmozorientationchange"] = !!(onmozorientationchange in screen);
+    
+    tests["window.screenX"] = get(window.screenX);
+    tests["window.screenY"] = get(window.screenY);
+    tests["window.mozInnerScreenX"] = get(window.mozInnerScreenX);
+    tests["window.mozInnerScreenY"] = get(window.mozInnerScreenY);
+    
+    
     test_runner.addTest(tests); // add all the tests at once
 }());
